84f414ee5abb952557905fcfc76bd87e5aa54756
Amanda Pipich <apipich2207@gmail.com>
2021-05-11 11:23:10 -0400

    created new function getForecast in JS
    beginning to pull daily forecast from API

--- src/index.js
+++ src/index.js
@@ -26,6 +26,15 @@ function formatDate(date) {
 
 formatDate();
 
+//
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "044e518b60304de98c4849e683a28d22";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showForecast);
+}
+
 // changing city, then change temperature
 // add humidity of weather, add windspeed
 
@@ -52,6 +61,7 @@ function showWeather(response) {
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function findCity(city) {
@@ -122,7 +132,8 @@ fLink.addEventListener("click", getFahrenheit);
 
 // forecast
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -144,5 +155,3 @@ function showForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-showForecast();
